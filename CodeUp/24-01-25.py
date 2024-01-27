0165ea12e8f278bd060f1139844d82a0baf712cb
hyeon seo <skybluehs0718@gmail.com>
2024-01-27 18:43:33 +0900

    codeup-py

--- CodeUp/24-01-25.py
+++ CodeUp/24-01-25.py
@@ -4,3 +4,45 @@ a = float(input())
 b = float(input())
 sum = a+b
 print(sum)
+#
+#6027
+a = int(input())
+print('%x'% a)
+#
+#6028
+a = int(input())
+print('%X'% a)
+#
+#6029
+a = int(input(), 16)
+print('%o'% a)
+#
+#6030
+a = ord(input())
+print(a)
+#
+#6031
+a = int(input())
+print(chr(a))
+#
+#6032
+a = int(input())
+print(-a)
+#
+#6033
+a = ord(input())
+print(chr(a+1))
+#
+#6034
+a, b = map(int, input().split())
+c = a-b
+print(c)
+#
+#6035
+a, b = map(float, input().split())
+c = a*b
+print(c)
+#
+#6036
+a, b = input().split()
+print(a*int(b))
